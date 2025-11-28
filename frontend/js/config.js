305d4ca4575af1753cd710ebed0be73b89914c00
opti89 <johnnypot99@gmail.com>
2025-11-28 11:42:39 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,6 @@
-const SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A";
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = https://regighceakyjsscojxku.supabase.co
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
