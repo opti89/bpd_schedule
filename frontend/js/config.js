e7bb65f9b6c63dc217fc49dfce7b6c8cfac8f801
opti89 <johnnypot99@gmail.com>
2025-11-28 14:21:20 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,6 @@
 
 const SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const SUPABASE_ANON_KEY = "sb_publishable_8L-OWPEJ85DlHXIqCorU4Q_pnoRIEuY";
+
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
