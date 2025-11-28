5fca5e60d3bae3d7d1f7c314681e632443778985
opti89 <johnnypot99@gmail.com>
2025-11-28 10:21:47 -0500

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A";
