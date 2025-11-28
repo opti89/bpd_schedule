b0b62234053b946fc83c98aa0a58ca9c08a71bcc
opti89 <johnnypot99@gmail.com>
2025-11-28 11:58:28 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,8 +1,9 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-// Put your values inside quotes
-const supabaseUrl = "https://regighceakyjsscojxku.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A"
 
-// Create the Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co"
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A"
+
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
