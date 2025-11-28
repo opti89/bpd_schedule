687aa966a286a4f72c932ab20080337ee3d4da28
opti89 <johnnypot99@gmail.com>
2025-11-28 14:33:56 -0500

    Delete frontend/js/config.js

--- frontend/js/config.js
+++
@@ -1,6 +0,0 @@
-
-const SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_8L-OWPEJ85DlHXIqCorU4Q_pnoRIEuY";
-
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
