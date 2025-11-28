e284ffd08eec5aef1610de3c32235a8a4693a6f3
opti89 <johnnypot99@gmail.com>
2025-11-28 12:18:35 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
-window.SUPABASE_URL = "https://regighceakyjsscojxku.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A";
+window.SUPABASE_URL = ("https://regighceakyjsscojxku.supabase.co");
+window.SUPABASE_ANON_KEY = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2lnaGNlYWt5anNzY29qeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNDEwNDksImV4cCI6MjA3OTkxNzA0OX0.xUNwNTdSF3xHf8FOAbTmhenAErN9Y2wBGJJEr-ilu9A");
 
 // Create global supabase client
 window.supabase = Supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
